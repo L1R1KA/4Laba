ed67a6fe6d93adae0cdd5680234e2e4a5bdce34d
L1R1KA <70565141+L1R1KA@users.noreply.github.com>
2020-12-08 00:31:07 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Tarelka tarelka1 = new Tarelka();
+        Televizor televizor1 = new Televizor();
+        Baton baton1 = new Baton();
+        Stol stol1 = new Stol();
+        Priceable[] priceables = {tarelka1, televizor1, baton1, stol1};
+
+        int sum = 0;
+        for (int i = 0; i < priceables.length; i++) {
+            sum += priceables[i].getPrice();
+
+
+
+
+
+        }
+        System.out.println(sum);
+
+    }
+}
